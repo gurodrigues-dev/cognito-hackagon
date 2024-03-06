29ef2711caa2cdcf61edf5d416c1294dee2514b9
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-06 00:46:38 -0300

    feat: init

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"gin/config"
+	"gin/internal/controllers"
+	"gin/internal/service"
+	"gin/repository"
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
-var Router * gin.Engine
+
+var Router *gin.Engine
+
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world!",
-		})
-	})
-	r.Run()
+
+	config, err := config.Load("config/config.yaml")
+	if err != nil {
+		log.Fatalf("error loading config: %s", err.Error())
+	}
+
+	repo, err := repository.NewPostgres()
+	if err != nil {
+		log.Fatalf("error creating repository: %s", err.Error())
+	}
+
+	redis, err := repository.NewRedisClient()
+	if err != nil {
+		log.Fatalf("error creating redis connection: %s", err.Error())
+	}
+
+	service := service.New(repo, redis)
+
+	controller := controllers.New(service)
+
+	log.Printf("initing service: %s", config.Name)
+	controller.Start()
+
 }
