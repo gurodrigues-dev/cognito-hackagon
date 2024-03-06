5394cf6db88cec8798cbe9e52f5fc1dcbfd6e572
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-06 00:46:47 -0300

    feat: charging vars

---
+++ config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Name     string `yaml:"name"`
+	Server   Server
+	Database Database
+	Cache    Cache
+}
+
+type Server struct {
+	Host   string `yaml:"host"`
+	Port   int    `yaml:"port"`
+	Secret string `yaml:"string"`
+}
+
+type Database struct {
+	User     string `yaml:"dbuser"`
+	Port     string `yaml:"dbport"`
+	Host     string `yaml:"dbhost"`
+	Password string `yaml:"dbpassword"`
+	Name     string `yaml:"dbname"`
+}
+
+type Cache struct {
+	Address  string `yaml:"address"`
+	Password string `yaml:"password"`
+}
+
+var config *Config
+
+func Load(filename string) (*Config, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var conf Config
+	if err := yaml.Unmarshal(data, &conf); err != nil {
+		return nil, err
+	}
+
+	config = &conf
+	return config, nil
+}
+
+func Get() *Config {
+	return config
+}
